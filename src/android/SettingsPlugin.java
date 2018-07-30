a18efa3c85d5b1eee066c4f7f18717f186011445
mohan1304 <mohanreddy.kmr@gmail.com>
2018-07-30 11:06:17 -0400

    Phonegap build error fix
    compileArmv7DebugJavaWithJavac - is not incremental (e.g. outputs have changed, no previous execution, etc.).
    /src/com/custom/settings/SettingsPlugin.java:21: error: cannot find symbol
                        Intent intent = new Intent(cordova.getContext(), SettingsMainActivity.class);
                                                          ^
      symbol:   method getContext()
      location: variable cordova of type CordovaInterface
    Note: Some input files use or override a deprecated API.
    Note: Recompile with -Xlint:deprecation for details.
    Note: Some input files use unchecked or unsafe operations.
    Note: Recompile with -Xlint:unchecked for details.
    1 error

--- src/android/SettingsPlugin.java
+++ src/android/SettingsPlugin.java
@@ -1,5 +1,6 @@
 package com.custom.settings;
 
+import android.content.Context;
 import android.content.Intent;
 
 import org.apache.cordova.*;
@@ -16,11 +17,12 @@ public class SettingsPlugin extends CordovaPlugin {
             String name = data.getString(0);
             //String message = "Hello, " + name;
             //callbackContext.success(message);
+            Context context = this.cordova.getContext();
             cordova.getThreadPool().execute(new Runnable() {
                 public void run() {
-                    Intent intent = new Intent(cordova.getContext(), SettingsMainActivity.class);
-                    cordova.getActivity().startActivity(intent);
-                    //callbackContext.success(message);
+                    Intent intent = new Intent(context, SettingsMainActivity.class);
+                    context.startActivity(intent);
+                    callbackContext.success(message);
                 }
             });
 
